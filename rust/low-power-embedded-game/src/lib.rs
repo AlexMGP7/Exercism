6e543cb5e8d58680a2447cbec3003c32f19472f5
Alexander Gonzalez <agonzalezpastena@gmail.com>
2023-02-20 17:17:54 -0400

    Low-Power Embedded Game exercism
    You are working on a game targeting a low-power embedded system and need to write several convenience functions which will be used by other parts of the game.

---
+++ rust/low-power-embedded-game/src/lib.rs
@@ -0,0 +1,27 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+use std::any::TypeId;
+
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    //unimplemented!("implement `fn divmod`");
+    (dividend / divisor, dividend % divisor)
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.enumerate()
+        // we do not care about the value for this filter, so we use _ - i is checked in the closure as we are filtering on even indexes
+        .filter(|(i, _)| i % 2 == 0)
+        // takes a closure & creates an iterator from the closure, in this instance it provides the value of the iteration as an iterator
+        .map(|(_i, v)| v)
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        //unimplemented!("implement `fn manhattan`")
+        let Position(x, y) = self;
+        x.abs() + y.abs()
+    }
+}
